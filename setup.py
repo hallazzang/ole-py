3fc5ef5d036c13fe6e591c0b779ec9b468789d1e
Hanjun Kim <hallazzang@gmail.com>
2019-09-30 19:49:03 +0900

    version up to 0.1.0
    fix #1

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
 import os
-from setuptools import setup
+from setuptools import find_packages, setup
 
 def get_long_description(path):
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    with open(path) as f:
+    with open(path, encoding='utf-8') as f:
         return f.read()
 
 setup(
     name='ole-py',
-    version='0.0.2',
+    version='0.1.0',
     url='https://github.com/hallazzang/ole-py',
     license='MIT',
     author='Hanjun Kim',
     author_email='hallazzang@gmail.com',
     description='Lightweight Microsoft OLE file parser in pure Python',
     long_description=get_long_description('README.rst'),
-    py_modules=['ole','utils'],
-    python_requires='>=3',
-    zip_safe=False,
-    platforms='any',
+    packages=find_packages(),
+    python_requires='>=3.4',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
