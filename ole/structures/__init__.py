023f4541dd4fb9e5caa656d6ddcacab4911d523f
Hanjun Kim <hallazzang@gmail.com>
2019-10-02 22:47:17 +0900

    create DirectoryEntry structure

--- ole/structures/__init__.py
+++ ole/structures/__init__.py
@@ -1,1 +1,2 @@
 from .file_header import FileHeader
+from .directory_entry import DirectoryEntry
---
+++ ole/structures/directory_entry.py
@@ -0,0 +1,17 @@
+from .structure import Structure
+
+
+class DirectoryEntry(Structure):
+    entry_name = Field('64s')
+    entry_name_len = Field('H')
+    object_type = Field('B')
+    color_flag = Field('B')
+    left_sibling_id = Field('I')
+    right_sibling_id = Field('I')
+    child_id = Field('I')
+    clsid = Field('16s')
+    state_bits = Field('I')
+    creation_time = Field('Q')
+    modified_time = Field('Q')
+    starting_sector_loc = Field('I')
+    stream_size = Field('Q')
