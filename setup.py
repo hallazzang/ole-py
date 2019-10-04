f179f4773547235beb2e5e1f417e2686d2b1ccd6
Hanjun Kim <hallazzang@gmail.com>
2019-10-04 22:43:58 +0900

    version up to 1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ole-py',
-    version='0.1.1',
+    version='1.0.0',
     url='https://github.com/hallazzang/ole-py',
     license='MIT',
     author='Hanjun Kim',
@@ -18,14 +18,13 @@ setup(
     long_description_content_type='text/markdown',
     include_package_data=True,
     packages=find_packages(),
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
